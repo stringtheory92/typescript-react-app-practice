ff8f8fa5935820bd936aac3888dafdf6cd47800b
Adam Kabak <adamkabak@yahoo.com>
2023-02-18 10:38:29 -0500

    reminder service, get/delete implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ReminderList from "./components/reminderList";
+import Reminder from "./models/reminder";
+import reminderService from "./services/reminder";
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+
+  useEffect(() => {
+    loadReminders();
+  }, []);
+
+  const loadReminders = async () => {
+    const reminders = await reminderService.getReminders();
+    setReminders(reminders);
+  };
+
+  const removeReminder = (id: number) => {
+    console.log(id);
+    setReminders(reminders.filter((reminder) => reminder.id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReminderList
+        items={reminders}
+        onRemoveReminder={removeReminder}
+      ></ReminderList>
     </div>
   );
 }
